646f03bdf259bcd67cb61c86dc56f1cbe42b8670
Lee Jia Keat <leejiakeat@gmail.com>
2023-08-26 21:26:59 +0800

    test image to text

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 from flask import request
 
+from imagetotext import describe_image
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+replicate_token = os.environ['REPLICATE_API_KEY']
+
 app = Flask(__name__)
 CORS(app)
 
@@ -22,6 +29,7 @@ def capture():
     f = open('./image.jpeg', 'wb')
     f.write(request.data)
     f.close()
+    describe_image('./image.jpeg', replicate_token)
     return '{"text": "Image file saved!"}'
 
 @app.route('/test', methods = ['POST'])
---
+++ imagetotext.py
@@ -0,0 +1,11 @@
+import os
+import replicate
+
+def describe_image(file_path, token):
+    replicate_client = replicate.Client(api_token=token)
+    output = replicate_client.run(
+        "daanelson/minigpt-4:b96a2f33cc8e4b0aa23eacfce731b9c41a7d9466d9ed4e167375587b54db9423",
+        input={"image": open(os.path.dirname(__file__) +  "\\" + file_path, "rb"),
+            "prompt": "what is this image, explain to me in detail?"}
+    )
+    print(output)
