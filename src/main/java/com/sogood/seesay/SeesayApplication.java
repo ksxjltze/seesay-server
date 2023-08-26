b6c93cddb22f82cbefec00ff7b331efaaaedc730
Lee Jia Keat <leejiakeat@gmail.com>
2023-08-26 14:23:30 +0800

    python magic

--- src/main/java/com/sogood/seesay/SeesayApplication.java
+++ src/main/java/com/sogood/seesay/SeesayApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class SeesayApplication {
@@ -18,13 +21,32 @@ public class SeesayApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
 		return args -> {
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
 
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
+			ProcessBuilder processBuilder = new ProcessBuilder("python", "./src/main/resources/scripts/test.py");
+			processBuilder.redirectErrorStream(true);
+
+			Process process = processBuilder.start();
+			int exitCode = process.waitFor();
+
+			System.out.println(exitCode);
+
+			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			StringBuilder builder = new StringBuilder();
+			String line = null;
+			while ( (line = reader.readLine()) != null) {
+				builder.append(line);
+				builder.append(System.getProperty("line.separator"));
 			}
+			String result = builder.toString();
+			System.out.println(result);
+
+//			System.out.println("Let's inspect the beans provided by Spring Boot:");
+//
+//			String[] beanNames = ctx.getBeanDefinitionNames();
+//			Arrays.sort(beanNames);
+//			for (String beanName : beanNames) {
+//				System.out.println(beanName);
+//			}
 		};
 	}
 
