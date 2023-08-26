4ddaf19f69e05efd6f85274a2c81554244369dce
Lee Jia Keat <leejiakeat@gmail.com>
2023-08-26 16:55:28 +0800

    why

--- src/main/java/com/sogood/seesay/SeesayApplication.java
+++ src/main/java/com/sogood/seesay/SeesayApplication.java
@@ -17,36 +17,36 @@ public class SeesayApplication {
 		SpringApplication.run(SeesayApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
-		return args -> {
-
-//			ProcessBuilder processBuilder = new ProcessBuilder("python", "./src/main/resources/scripts/test.py");
-//			processBuilder.redirectErrorStream(true);
+//	@Bean
+//	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+//		return args -> {
 //
-//			Process process = processBuilder.start();
-//			int exitCode = process.waitFor();
+////			ProcessBuilder processBuilder = new ProcessBuilder("python", "./src/main/resources/scripts/test.py");
+////			processBuilder.redirectErrorStream(true);
+////
+////			Process process = processBuilder.start();
+////			int exitCode = process.waitFor();
+////
+////			System.out.println(exitCode);
+////
+////			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+////			StringBuilder builder = new StringBuilder();
+////			String line = null;
+////			while ( (line = reader.readLine()) != null) {
+////				builder.append(line);
+////				builder.append(System.getProperty("line.separator"));
+////			}
+////			String result = builder.toString();
+////			System.out.println(result);
 //
-//			System.out.println(exitCode);
-//
-//			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-//			StringBuilder builder = new StringBuilder();
-//			String line = null;
-//			while ( (line = reader.readLine()) != null) {
-//				builder.append(line);
-//				builder.append(System.getProperty("line.separator"));
-//			}
-//			String result = builder.toString();
-//			System.out.println(result);
-
-//			System.out.println("Let's inspect the beans provided by Spring Boot:");
-//
-//			String[] beanNames = ctx.getBeanDefinitionNames();
-//			Arrays.sort(beanNames);
-//			for (String beanName : beanNames) {
-//				System.out.println(beanName);
-//			}
-		};
-	}
+////			System.out.println("Let's inspect the beans provided by Spring Boot:");
+////
+////			String[] beanNames = ctx.getBeanDefinitionNames();
+////			Arrays.sort(beanNames);
+////			for (String beanName : beanNames) {
+////				System.out.println(beanName);
+////			}
+//		};
+//	}
 
 }
