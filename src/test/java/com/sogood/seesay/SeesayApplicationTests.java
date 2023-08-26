ae3ff77592caf534e84a6fdc063fb407b8ed66d8
Lee Jia Keat <leejiakeat@gmail.com>
2023-08-26 13:55:52 +0800

    hello world

---
+++ src/test/java/com/sogood/seesay/SeesayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sogood.seesay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeesayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
